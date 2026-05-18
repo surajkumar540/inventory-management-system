b0cd329ce056837a79eea4d2e77ce2df1817df89
Suraj Kumar <surajgautam56878@gmail.com>
2026-05-19 00:20:13 +0530

    changed backend path which have running api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,29 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// // ✅ ALWAYS use latest token
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return req;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://13.51.70.1:5000/api",
 });
 
-// ✅ ALWAYS use latest token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
